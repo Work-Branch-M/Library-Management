b656ab56388b41702cec1a1b36649bd6a3fb8df0
Work-Branch <al.arman.ovi.sohana@gmail.com>
2025-10-02 19:37:15 +0600

    Delete BorrowerInfo.cpp

--- BorrowerInfo.cpp
+++
@@ -1,6 +0,0 @@
-#include "BorrowerInfo.h"
-
-void BorrowerInfo::displayInfo() const {
-    std::cout << borrowerName << " (" << studentId << ") -> "
-              << bookId << ": " << bookName << " [" << category << "]\n";
-}
